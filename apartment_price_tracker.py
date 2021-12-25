186538b8c8061289f0a81af9eb17091d003b5f34
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-12-24 21:55:27 -0500

    Implement scraper for 2010 West End, implement min/max/range price getters

--- apartment_price_tracker.py
+++ apartment_price_tracker.py
@@ -26,9 +26,7 @@ class ApartmentPriceTracker:
 
         return soup
 
-    def get_prices(self, apt_name) -> list[float]:
-        soup = self.get_soup(apt_name)
-
+    def get_prices(self, apt_name: str) -> list[float]:
         apt_prices = list()
 
         if apt_name == "Aertson Midtown":
@@ -36,6 +34,8 @@ class ApartmentPriceTracker:
             # Up two is div tag with class 'card'
             # 'card' has child div tag with class 'card-body'
             # 'card-body' has child div tag with child p tag with text of price
+
+            soup = self.get_soup(apt_name)
 
             floorplan_title_elems = soup.find_all("h2", class_="card-title", string=re.compile("^\s*2"))  # Only targets 2 bed floorplans
             floorplan_card_elems = [title_elem.parent.parent for title_elem in floorplan_title_elems]
@@ -50,23 +50,58 @@ class ApartmentPriceTracker:
                 apt_prices.append(price)
 
         elif apt_name == "Morris":
-            # TODO
-
             # Look for 4th (last) li tag in ul with class 'card__stats'
             # Text of li tag is 'Starting at $[price]'
 
-            pass
-        elif apt_name == "2010 West End":
-            # TODO
+            # TODO: Uh-oh! DYNAMIC WEBSITE! Selenium to the rescue!
 
+            soup = self.get_soup(apt_name)
+            
+            floorplan_card_elems = soup.find_all("ul", class_="card__stats")
+            floorplan_price_elems = [card_elem.find_all("li")[-1] for card_elem in floorplan_card_elems]
+
+            # RegEx pattern for parsing price value
+            pattern = re.compile(r"\$(\S*)\s")
+            
+            for price_elem in floorplan_price_elems:
+                match = pattern.search(price_elem.text)
+                price = float(match.group(1).replace(",", "")) if match is not None else 0  # Remove , characters and convert to float
+                apt_prices.append(price)
+
+        elif apt_name == "2010 West End":
             # Each floorplan is represented by a chart with each row being a unit
             # Charts are contained in div tag with class 'additional-content__rows'
             # Each row is div tag with class 'row'
             # Price is text of span tag within last child div tag of a row
 
-            pass
+            soup = self.get_soup(apt_name)
+
+            floorplan_unit_elems = soup.find_all("div", class_="additionally-content__row")
+            floorplan_price_elems = [unit_elem.find_all("div")[-1].find("span") for unit_elem in floorplan_unit_elems]
+
+            # RegEx pattern for matching characters to remove
+            pattern = re.compile(r"[$,]")
+            
+            for price_elem in floorplan_price_elems:
+                price_str = re.sub(pattern, "", price_elem.text)  # Remove $ and , characters
+                
+                try:
+                    price = float(price_str)
+                except ValueError:
+                    price = 0
+                    
+                apt_prices.append(price)
         
         return apt_prices
+
+    def get_min_price(self, apt_name: str) -> float:
+        return min(self.get_prices(apt_name))
+
+    def get_max_price(self, apt_name: str) -> float:
+        return max(self.get_prices(apt_name))
+    
+    def get_price_range(self, apt_name: str) -> tuple(float, float):
+        return (self.get_min_price(apt_name), self.get_max_price(apt_name))
 
 
 if __name__ == "__main__":
