ab23f989943c4d65ed2db53546288c3be857d83b
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-12-24 20:12:51 -0500

    Implement scraping for Aertson Midtown apartments

--- apartment_price_tracker.py
+++ apartment_price_tracker.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 import requests
 
 APARTMENTS = {
@@ -13,31 +14,45 @@ class ApartmentPriceTracker:
         self.apartments = apts  # APT_NAME: APT_URL
         return
 
-    def get_prices(self, apt_name) -> list[float]:
+    def get_soup(self, apt_name) -> BeautifulSoup:
         try:
             assert apt_name in self.apartments
         except AssertionError:
             print(f"Price tracking for '{apt_name}' is not supported.")
-            return list()
+            return bytes()
 
-        response = requests.get(self.apartments[apt_name])
-        soup = BeautifulSoup(response.content, "html.parser")
+        web_page = requests.get(self.apartments[apt_name]).content
+        soup = BeautifulSoup(web_page, "html.parser")
+
+        return soup
+
+    def get_prices(self, apt_name) -> list[float]:
+        soup = self.get_soup(apt_name)
 
         apt_prices = list()
 
         if apt_name == "Aertson Midtown":
-            # TODO
-            
             # Look for h2 tag with class 'card-title' (h2 text specifies floorplan name)
             # Up two is div tag with class 'card'
             # 'card' has child div tag with class 'card-body'
             # 'card-body' has child div tag with child p tag with text of price
 
-            pass
+            floorplan_title_elems = soup.find_all("h2", class_="card-title", string=re.compile("^\s*2"))  # Only targets 2 bed floorplans
+            floorplan_card_elems = [title_elem.parent.parent for title_elem in floorplan_title_elems]
+            floorplan_price_elems = [card_elem.find("div", class_="card-body").find("p") for card_elem in floorplan_card_elems]
+            
+            # RegEx pattern for parsing price value
+            pattern = re.compile(r"\$(\S*)\s")
+            
+            for price_elem in floorplan_price_elems:
+                match = pattern.search(price_elem.text)
+                price = float(match.group(1).replace(",", "")) if match is not None else 0  # Remove , characters and convert to float
+                apt_prices.append(price)
+
         elif apt_name == "Morris":
             # TODO
 
-            # Look for 4th li tag in ul with class 'card__stats'
+            # Look for 4th (last) li tag in ul with class 'card__stats'
             # Text of li tag is 'Starting at $[price]'
 
             pass
@@ -57,7 +72,8 @@ class ApartmentPriceTracker:
 if __name__ == "__main__":
     apt_price_tracker = ApartmentPriceTracker()
 
-    print("Minimum prices found:")
-    print("---------------------")
-    for apt_name in APARTMENTS:
-        print(f"{apt_name}: ${min(apt_price_tracker.get_prices(apt_name)):.2f}")
+    print("-----------------")
+    print("Apartment Prices:")
+    print("-----------------")
+    for apt_name in apt_price_tracker.apartments:
+        print(f"{apt_name}: {[f'${price:.2f}' for price in apt_price_tracker.get_prices(apt_name)]}")
